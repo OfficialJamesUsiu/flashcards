b396826eb802bfb583f48ec556cda7a32d2b43a5
OfficialJamesUsiu <jaycomputertechnologies@gmail.com>
2020-01-31 12:02:35 -0500

    Updates

---
+++ precache-manifest.08cdeb818ee8c2d41266724b607731a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15b96bcce8cac5d1d29af8fd12a34776",
+    "url": "/flashcards/index.html"
+  },
+  {
+    "revision": "cee9766c42e442571f39",
+    "url": "/flashcards/static/css/main.a71696b9.chunk.css"
+  },
+  {
+    "revision": "f32d6435eb5ddc9222b2",
+    "url": "/flashcards/static/js/2.8484ed74.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/flashcards/static/js/2.8484ed74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cee9766c42e442571f39",
+    "url": "/flashcards/static/js/main.36d3f9b4.chunk.js"
+  },
+  {
+    "revision": "7e14da0d7f813acbe212",
+    "url": "/flashcards/static/js/runtime-main.b7770e97.js"
+  }
+]);
